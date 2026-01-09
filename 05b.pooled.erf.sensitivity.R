5656ec2c8d3e81f366d3eb78ffdbc9bf0721b755
Veronika Huber <129281378+veronikahuber@users.noreply.github.com>
2026-01-09 11:21:12 +0100

    Update header

--- 05b.pooled.erf.sensitivity.R
+++ 05b.pooled.erf.sensitivity.R
@@ -1,11 +1,10 @@
-#################################################################################
+#####################################################################################
 # R code for the analysis in 
 
-# Huber, V., et al. Life expectancy gains mask rising trends in heat-related
-# excess mortality attributable to climate change. 
-# Nature Communications, [to be added] (2025), doi: [to be added]
-
-#################################################################################
+# Huber, V., Breitner-Busch, S., Feldbusch, H. et al. Improvements in life expectancy 
+# mask rising trends in heat-related excess mortality attributable to climate change. 
+# Nat Commun 16, 11632 (2025). https://doi.org/10.1038/s41467-025-66681-0
+######################################################################################
 
 ###########################################################################
 # PLOT SUPPLEMENTARY FIG.9
@@ -496,5 +495,6 @@ box(lty=1,lwd=1)
 text(min(tmeancountrysum$tmeancountry),4,labels="d")
 
 dev.off()
+
 
 ####
