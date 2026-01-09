1fbf46e0022d6fef6d8f4e7fd8687ee49dbee24e
Veronika Huber <129281378+veronikahuber@users.noreply.github.com>
2026-01-09 11:19:23 +0100

    Update header

--- 01b.counterfactual.sensitivity.R
+++ 01b.counterfactual.sensitivity.R
@@ -1,11 +1,10 @@
-#################################################################################
+#####################################################################################
 # R code for the analysis in 
 
-# Huber, V., et al. Life expectancy gains mask rising trends in heat-related
-# excess mortality attributable to climate change. 
-# Nature Communications, [to be added] (2025), doi: [to be added]
-
-#################################################################################
+# Huber, V., Breitner-Busch, S., Feldbusch, H. et al. Improvements in life expectancy 
+# mask rising trends in heat-related excess mortality attributable to climate change. 
+# Nat Commun 16, 11632 (2025). https://doi.org/10.1038/s41467-025-66681-0
+######################################################################################
 
 ##################################################################################
 # COMPUTE COUNTERFACTUAL DAILY MEAN TEMPERATURES 
@@ -236,6 +235,7 @@ for (i in seq(cities)){
 dev.off()
 
 #############
+
 
 
 
