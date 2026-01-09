bc4ace235b5beb77c2bde64cde056c6f512115ad
Veronika Huber <129281378+veronikahuber@users.noreply.github.com>
2026-01-09 11:22:51 +0100

    Update header

--- 09.plot.temp.trend.R
+++ 09.plot.temp.trend.R
@@ -1,11 +1,10 @@
-#################################################################################
+#####################################################################################
 # R code for the analysis in 
 
-# Huber, V., et al. Life expectancy gains mask rising trends in heat-related
-# excess mortality attributable to climate change. 
-# Nature Communications, [to be added] (2025), doi: [to be added]
-
-#################################################################################
+# Huber, V., Breitner-Busch, S., Feldbusch, H. et al. Improvements in life expectancy 
+# mask rising trends in heat-related excess mortality attributable to climate change. 
+# Nat Commun 16, 11632 (2025). https://doi.org/10.1038/s41467-025-66681-0
+######################################################################################
 
 ##############################################################################
 # 1) PLOT ANNUAL MEAN SUMMER TEMPERATURES PER CITY
@@ -286,3 +285,4 @@ text(0,ylim[2],labels="b")
 dev.off()
 
 ########################
+
